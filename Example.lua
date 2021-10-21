10d7b42238cde2cb782f03f78f435757b1b15bf0
ScriptyDev <77843599+aaa826@users.noreply.github.com>
2021-10-21 17:27:08 -0400

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,9 +1,9 @@
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/aaa826/NoobUILibrary/main/NoobUI.lua"))() --Starts up the UI Lib
 UI:Create("GUIName", "GameName") --change GUIName to your hub name and GameName to whatever game u want it to be
 local Windows = Win:Page("PageName", true) --set to false if its your 2nd page)
-Windows:NewButton("ButtonName", true) --New Button
-Windows:NewToggle("ToggleName", true) --New Toggle
-Windows:NewDropdown("Dropdownname", {"DropdownItem1", "DropdownItem2", "DropdownItem3"}, true)
-Windows:NewSlider("Xd", true)
+Windows:NewButton("ButtonName", function() --New Button
+Windows:NewToggle("ToggleName", function() --New Toggle
+Windows:NewDropdown("Dropdownname", {"DropdownItem1", "DropdownItem2", "DropdownItem3"}, function()
+Windows:NewSlider("Xd", function()
 local Player = Win:Page("PageName2", false) -- notice the difference?
-Player:NewToggle("test", true)
+Player:NewToggle("test", function()
